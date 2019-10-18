e083dc51d1ac21bd84b2679f73c79aad9bd40d1e
ShaoCapo <56005343+ShaoCapo@users.noreply.github.com>
2019-10-18 09:28:58 +0200

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+
+    public static int numMinMutaciones(String cadena1, String cadena2) {
+        //SUTITUIR ESTA LINEA E IMPLEMENTAR
+        return -1;
+    }
+
+    /**
+     * en un vector se acceden a sus elementos: v[i] y con este string se hace: v.charAt(i)
+     */
+}
